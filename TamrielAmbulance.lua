6e49dd8c5caeb0a623da03162e5065c57162eeb5
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2022-10-06 21:22:56 +0200

    Updating API version to Lost Depths

--- TamrielAmbulance.lua
+++ TamrielAmbulance.lua
@@ -5,7 +5,7 @@ TamrielAmbulance.name = "TamrielAmbulance"
 TamrielAmbulance.prettyName = "Tamriel Ambulance"
 TamrielAmbulance.coloredName = "|cff0000Tamriel |c000000Ambulance|r"
 TamrielAmbulance.author = "|cff6600Infenix|r"
-TamrielAmbulance.version = "1.2.6"
+TamrielAmbulance.version = "1.2.7"
 TamrielAmbulance.website = "https://github.com/ImInfenix/TamrielAmbulance"
 
 -------------------------------------------------------------------------------------------------------------------------
